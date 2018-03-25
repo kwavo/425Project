659ea627e0b1bb589a84ed105426793a97ac6365
DESKTOP-NF2K73N\jkwak <kwakjonathan7@gmail.com>
2018-03-25 18:37:08 -0400

    got api to connect

--- 425Project/app/src/main/java/com/cnit355/myles/a425project/MainActivity.java
+++ 425Project/app/src/main/java/com/cnit355/myles/a425project/MainActivity.java
@@ -1,18 +1,37 @@
 package com.cnit355.myles.a425project;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText test;
+    TextView question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button3);
+        test = findViewById(R.id.editText);
+        question = findViewById(R.id.textView2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,5 +39,61 @@ public class MainActivity extends AppCompatActivity {
                 new Retrieve().execute();
             }
         });
+    }
+
+    class Retrieve extends AsyncTask<Void, Void, String> {
+        private Exception exception;
+
+        protected void onPreExecute() {
+            //responseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+            //String email = emailText.getText().toString();
+            // Do some validation here
+
+            try {
+                URL url = new URL("https://opentdb.com/api.php?amount=10&type=multiple");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+
+            Log.i("INFO", response);
+            //responseView.setText(response);
+            try {
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                JSONArray jsonArray = new JSONArray(object.getString("results"));
+                JSONObject x = (JSONObject)jsonArray.get(0);
+                String name = x.getString("question");
+                Log.i("question : " , name);
+                question.setText(name);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
